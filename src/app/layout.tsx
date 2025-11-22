a05ae6b0f105be5c82e81909d3a7bd758e1d6f3a
ARVIND Dara <ravibishnoirb047400@gmail.com>
2025-11-22 09:01:24 +0000

    Try fixing this error: `Runtime Error: Error: useFirebase must be used w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import { FirebaseClientProvider } from '@/firebase';
 
 export const metadata: Metadata = {
   title: 'AI Code Architect',
@@ -29,7 +30,9 @@ export default function RootLayout({
         />
       </head>
       <body>
+        <FirebaseClientProvider>
           {children}
+        </FirebaseClientProvider>
         <Toaster />
       </body>
     </html>
